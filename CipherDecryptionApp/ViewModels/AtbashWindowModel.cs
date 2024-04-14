9376766e7d0e196a091eb9478d594fd9583b76c7
Seb <sedea23@student.sdu.dk>
2024-04-14 18:01:46 +0200

    it's not working yet

---
+++ CipherDecryptionApp/ViewModels/AtbashWindowModel.cs
@@ -0,0 +1,51 @@
+using System.Reactive;
+using ReactiveUI;
+
+namespace CipherDecryptionApp.ViewModels
+
+{
+    public class AtBashWindow : ViewModelBase
+    {
+        private string _input = string.Empty;
+        private string _output = string.Empty;
+
+        public ReactiveCommand<Unit, Unit> EncodeCommand { get; }
+        public ReactiveCommand<Unit, Unit> DecodeCommand { get; }
+
+        public AtBashWindow()
+        {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.Input,
+                (input) => !string.IsNullOrWhiteSpace(input)
+            );
+
+            EncodeCommand = ReactiveCommand.Create(ExecuteEncodeCommand, isValidObservable);
+            DecodeCommand = ReactiveCommand.Create(ExecuteDecodeCommand, isValidObservable);
+        }
+
+        public string Input
+        {
+            get => _input;
+            set => this.RaiseAndSetIfChanged(ref _input, value);
+        }
+
+
+        public string Output
+        {
+            get => _output;
+            set => this.RaiseAndSetIfChanged(ref _output, value);
+        }
+
+        private void ExecuteEncodeCommand()
+        {
+            ICipher cipher = new AtbashCipher();
+            Output = cipher.Encode(Input);
+        }
+
+        private void ExecuteDecodeCommand()
+        {
+            ICipher cipher = new AtbashCipher();
+            Output = cipher.Decode(Input);
+        }
+    }
+}
---
+++ CipherDecryptionApp/Views/AtbashWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CipherDecryptionApp.Views
+{
+    public partial class AtbashWindow : UserControl
+    {
+        public AtbashWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
